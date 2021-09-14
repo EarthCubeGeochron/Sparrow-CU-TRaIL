663c81bb710a630f067fd2b54ddad21fee53b4c6
Daven Quinn <dev@davenquinn.com>
2021-09-14 04:15:33 -0500

    Added basic image finding ability

--- plugins/grain_image_importer/__init__.py
+++ plugins/grain_image_importer/__init__.py
@@ -1,5 +1,5 @@
 import sparrow
-from sparrow.import_helpers import BaseImporter
+from sparrow.import_helpers import BaseImporter, SparrowImportError
 from os import environ
 from pathlib import Path
 from rich import print
@@ -9,13 +9,32 @@ class ImageImporter(BaseImporter):
     def run(self):
         data_dir = environ.get("SPARROW_DATA_DIR", None)
         file_list = Path(str(data_dir)).glob("**/*.tif")
-        self.iterfiles(file_list)
+        # Get rid of copied files
+        filtered_list = (f for f in file_list if not f.stem.endswith("_copy"))
+        self.iterfiles(filtered_list, fix_errors=True)
 
     def import_datafile(self, fn, rec, **kwargs):
         """
         Import an original data file
         """
-        print(fn)
+        sample_name = fn.stem
+        # The last part of the filename seems like an image index
+        if name[-1] in "abcdef":
+            sample_name = sample_name[:-1]
+        print(name)
+        db = sparrow.get_database()
+        Sample = db.model.sample
+
+        # Get or create a sample using SQLAlchemy methods
+        sample = (
+            db.session.query(Sample)
+            .filter(Sample.name.ilike(f"{sample_name}%"))
+            .one_or_none()
+        )
+        if sample is None:
+            sample = Sample(name=sample_name)
+
+        raise SparrowImportError("Refusing to import")
 
 
 @sparrow.task()
