f3fa70f36f383911fc8df997a028ee519faa440a
Daven Quinn <dev@davenquinn.com>
2021-09-14 01:25:09 -0500

    Switched to prestart script

--- Makefile
+++
@@ -1,7 +0,0 @@
-
-# Build custom docker base images before install
-# The base image depends on the sparrowdata/backend image
-# so we have to build this image first
-all:
-	sparrow build backend
-	docker build -t sparrow_trail_backend:latest base-image
--- sparrow-config.sh
+++ sparrow-config.sh
@@ -2,7 +2,7 @@
 
 export SPARROW_BACKUP_DIR="$SPARROW_CONFIG_DIR/backups"
 export SPARROW_LAB_NAME="TRaIL"
-export SPARROW_VERSION=">=2.0.0.beta1"
+export SPARROW_VERSION=">=2.0.0.beta13"
 
 #export SPARROW_SITE_CONTENT="$PROJECT_DIR/site-content"
 
@@ -13,5 +13,12 @@ export SPARROW_DATA_DIR="$SPARROW_CONFIG_DIR/TRaIL-Data"
 # Plugins
 export SPARROW_PLUGIN_DIR="$SPARROW_CONFIG_DIR/plugins"
 
+# Load secret values (not tracked in version control)
 overrides="${SPARROW_CONFIG_DIR}/sparrow-secrets.sh"
 [ -f "$overrides" ] && source "$overrides"
+
+# We need to build our own base image for OpenCV support
+# Note, you must build the backend image by running `make` in this repository
+# before running `sparrow up`. We are working on making this type
+# of substitution simpler.
+export SPARROW_BACKEND_IMAGE="sparrow_trail_backend:latest"
