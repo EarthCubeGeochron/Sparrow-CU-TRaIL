db5a98ac8805e0a2f0b1e3862032208394c039d1
Daven Quinn <dev@davenquinn.com>
2021-09-14 02:42:20 -0500

    Updated prestart script

--- sparrow-prestart.sh
+++ sparrow-prestart.sh
@@ -1,6 +1,4 @@
 # Build the backend image before we run sparrow up
-# We can do this in the config file becaues it's actually a shell script...
-# We should do this after loading all relevant environment variables
 # (this only works in Sparrow >= 2.0.0.beta14, and we might find a better way
 #  to do it in the future)
 echo "Building custom backend image..." >&2
