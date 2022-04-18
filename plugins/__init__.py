f9e9c6e0669b18939a0c26f1442e88b19959907b
Peter E. Martin <76170591+Peter-E-Martin@users.noreply.github.com>
2022-04-17 18:55:31 -0600

    Initial commit for plugin to get lab IDs
    New plugin that reads in an excel file listing desired sample names and returns the lab ID(s) for each sample name, and the date that the grain was picked

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,6 +5,7 @@ from .import_data.ArchiveImporter import TRaILImporter
 from .import_data.pickingImport import TRaILpicking
 from .import_data.heliumImport import TRaILhelium
 from .import_data.icpmsImport import TRaILicpms
+from .export_data.sampleID_export import LabID_exporter
 
 from sparrow import get_app
 from click import secho
@@ -38,3 +39,9 @@ def import_full_data():
     app = get_app()
     data_dir = Path("/data")
     TRaILImporter(app, data_dir)
+    
+@task(name='export-ID')
+def export_ID():
+    app = get_app()
+    data_dir = Path("/data")
+    LabID_exporter(app, data_dir)
---
+++ plugins/export_data/sampleID_export.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 17 16:51:18 2022
+
+@author: Peter
+"""
+
+from sparrow.import_helpers import BaseImporter
+import pandas as pd
+import glob
+import datetime
+
+class LabID_exporter(BaseImporter):
+    def __init__(self, app, data_dir, **kwargs):
+        super().__init__(app)
+        self.file = glob.glob(str(data_dir)+'/ExportSampleID/exportID.xlsx')[0]
+        self.get_IDs(self.file, data_dir)
+        
+    def get_IDs(self, file, data_dir):
+        samples = pd.read_excel(self.file)
+        
+        lab_ids = []
+        sample_ids = []
+        picking_dates = []
+        for name in samples['Name']:
+            lab_id = self.db.session.query(self.db.model.sample.lab_id).filter_by(name=name).all()
+            sample_id = self.db.session.query(self.db.model.sample.id).filter_by(name=name).all()
+            lab_ids.append(lab_id)
+            sample_ids.append(sample_id)
+        for n, l in enumerate(sample_ids):
+            picking_dates.append([])
+            for i in l:
+                picking_date = self.db.session.query(self.db.model.session.date).filter_by(sample_id=i[0], technique='Picking Information').all()
+                picking_dates[n].append(picking_date[0][0])
+        for n, l in enumerate(picking_dates):
+            picking_dates[n] = ', '.join([i.strftime('%Y-%m-%d') for i in l])
+        for n, l in enumerate(lab_ids):
+            # remove extraneous tuples and join to string for easier reading
+            lab_ids[n] = ', '.join([i[0] for i in l])
+        samples['lab_id'] = lab_ids
+        samples['picking date(s)'] = picking_dates
+        # print(samples)
+        samples.to_excel(str(data_dir)+'/ExportSampleID/sample_ids.xlsx')
+        
+        return
