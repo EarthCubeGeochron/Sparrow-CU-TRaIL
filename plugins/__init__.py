defc8f3edf12549002683349ab69b5ca77c6c1aa
Peter E. Martin <76170591+Peter-E-Martin@users.noreply.github.com>
2022-06-07 19:38:26 -0600

    rearrange order of tasks
    Group tasks more logically in the __init__.py file

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,9 +5,9 @@ from .import_data.pickingImport import TRaILpicking
 from .import_data.heliumImport import TRaILhelium
 from .import_data.icpmsImport import TRaILicpms
 from .export_data.sampleID_export import LabID_exporter
+from .export_data.create_publication_table import PublicationTable_exporter
 from .manipulate_data.dataReduction import TRaILdatecalc
 from .manipulate_data.AddSampleNote import AddSampleNote
-from .manipulate_data.printFullSample import TRaILprintsample
 
 from sparrow import get_app
 from click import secho
@@ -17,7 +17,13 @@ from pathlib import Path
 @task(name='say-hello')
 def say_hello():
     secho('Hello World, I am a Sparrow Task', fg='green')
-
+    
+@task(name='import-archive')
+def import_full_data():
+    app = get_app()
+    data_dir = Path('/data')
+    TRaILImporter(app, data_dir)
+    
 @task(name='import-picking-info')
 def import_picking():
     app = get_app()
@@ -35,19 +41,7 @@ def import_icpms():
     app = get_app()
     data_dir = Path('/data')
     TRaILicpms(app, data_dir)
-    
-@task(name='import-archive')
-def import_full_data():
-    app = get_app()
-    data_dir = Path('/data')
-    TRaILImporter(app, data_dir)
-    
-@task(name='export-ID')
-def export_ID():
-    app = get_app()
-    data_dir = Path('/data')
-    LabID_exporter(app, data_dir)
-    
+        
 @task(name='calculate-dates')
 def get_date():
     app = get_app()
@@ -60,9 +54,14 @@ def add_note():
     data_dir = Path('/data')
     AddSampleNote(app, data_dir)
     
-    
-@task(name='print-sample')
-def print_sample():
+@task(name='export-ID')
+def export_ID():
     app = get_app()
     data_dir = Path('/data')
-    TRaILprintsample(app, data_dir)
+    LabID_exporter(app, data_dir)
+
+@task(name='export-table')
+def export_table():
+    app = get_app()
+    data_dir = Path('/data')
+    PublicationTable_exporter(app, data_dir)
