50f11c3da890b5ee0c29a0753df2f6c090aa5e2a
Peter E. Martin <76170591+Peter-E-Martin@users.noreply.github.com>
2022-06-13 11:22:49 -0600

    Improve handling of NaN and 0 values

--- plugins/import_data/icpmsImport.py
+++ plugins/import_data/icpmsImport.py
@@ -4,36 +4,64 @@ import glob
 from rich import print
 import pandas as pd
 import re
+from dateutil import parser
 
 # Make datum using info in yaml file
 def make_datum(row, isotope):
     unit = re.search(r'\((\w+)\)', isotope).group(1)
     return {'value': row[isotope],
             'error': row.iloc[row.index.get_loc(isotope)+1],
-            'type': {'parameter': isotope.split(' (')[0], 'unit': unit}}
+            'type': {'parameter': isotope.split(' (')[0]+ ' (±2σ)', 'unit': unit}}
 
 def make_ppm(data, dim_mass_val, dim_mass_err):
     ppm = (data['value']/dim_mass_val)*1000
     # Get ppm uncertainty from combination of mass and nuclide amount uncertainty
-    ppm_s = ppm*((data['error']/data['value'])**2+(dim_mass_err/dim_mass_val)**2)**(1/2)
-    return {'value': ppm, 'error': ppm_s,
+    try:
+        ppm_s = ppm*(((data['error']/2)/data['value'])**2+((dim_mass_err/2)/dim_mass_val)**2)**(1/2)
+    except:
+        ppm_s = 0
+    return {'value': ppm, 'error': ppm_s*2,
             'type': {'parameter': data['type']['parameter'], 'unit': 'ppm'}}
 
 class TRaILicpms(BaseImporter):
     def __init__(self, app, data_dir, **kwargs):
         super().__init__(app)
-        file_list = glob.glob(str(data_dir)+'/icpmsData/*.xlsx')
+        file_list = glob.glob(str(data_dir)+'/icpmsData/*.txt')
         file_list = [f for f in file_list if '$' not in f]
         
-        # Get Dimensional mass ID
-        self.dim_mass_id = (self.db.session
-                            .query(self.db.model.datum_type)
-                            .filter_by(parameter='Dimensional mass')
-                            .all())[0].id
         self.iterfiles(file_list, **kwargs)
-        
+    
+    def query_analysis(self, lab_id, analysis_name):
+        Session = self.db.model.session
+        Sample = self.db.model.sample
+        Analysis = self.db.model.analysis
+        res = (self.db.session.query(Analysis)
+               .join(Session)
+               .join(Sample)
+               .filter(Sample.lab_id == lab_id)
+               .filter(Analysis.analysis_type == analysis_name)
+               .first())
+        return res
+
+    def query_datum(self, lab_id, datum_param):
+        Session = self.db.model.session
+        Sample = self.db.model.sample
+        Analysis = self.db.model.analysis
+        Datum = self.db.model.datum
+        DatumType = self.db.model.datum_type
+        res = (self.db.session.query(Datum)
+               .join(Analysis)
+               .join(Session)
+               .join(Sample)
+               .join(DatumType)
+               .filter(Sample.lab_id == lab_id)
+               .filter(DatumType.parameter == datum_param)
+               .first())
+        return res
+
     def import_datafile(self, fn, rec, **kwargs):
-        data = pd.read_excel(fn)
+        # data = pd.read_excel(fn)
+        data = pd.read_csv(fn, delimiter = '\t')
         
         # Iterate through rows
         for ix, row in data.iterrows():
@@ -58,7 +86,7 @@ class TRaILicpms(BaseImporter):
                       'not found. Double-check that the IDs match.\n')
                 return
             # Genearate correct date format
-            date = row['Date'].to_pydatetime().isoformat()
+            date = parser.parse(row['Date'])
             # Get list of columns to make datum with. Identify which columns are isotopes based on presence
             # of prentheses, which indicate that there is a unit to pull out
             isotopes = [i for i in row.index if '(' in i and 'lank' not in i]
@@ -86,28 +114,17 @@ class TRaILicpms(BaseImporter):
             self.db.load_data('session', session_dict)
             
             # look for whether a dimensional mass is recorded in Sparrow to permit ppm conversion
-            try:
-                sample_int_id = (self.db.session
-                                 .query(self.db.model.sample)
-                                 .filter_by(lab_id=sample_id)
-                                 .first()).id
-                derived = (self.db.session
-                           .query(self.db.model.session)
-                           .filter_by(sample_id=sample_int_id,
-                                      technique='Dates and other derived data')
-                           .first())
-                analysis = (self.db.session
-                            .query(self.db.model.analysis)
-                            .filter_by(session_id = derived.id,
-                                       analysis_type='Rs, mass, concentrations')
-                            .first())
-                dim_mass = (self.db.session
-                            .query(self.db.model.datum)
-                            .filter_by(type = self.dim_mass_id,
-                                       analysis=analysis.id)
-                            .first())
-                self.add_ppm(raw_data, dim_mass, analysis)
-            except AttributeError:
+            ppm_analysis = self.query_analysis(sample_id, 'Rs, mass, concentrations')
+            dim_mass = self.query_datum(sample_id, 'Dimensional mass (±2σ)')
+            ft_analysis = self.query_analysis(sample_id, 'Alpha ejection correction values')
+            Fts = {'238U Ft (±2σ)': None, '235U Ft (±2σ)': None, '232Th Ft (±2σ)': None, '147Sm Ft (±2σ)': None}
+            for Ft in Fts:
+                Fts[Ft] = self.query_datum(sample_id, Ft)
+            if dim_mass:
+                self.add_ppm(raw_data, dim_mass, ppm_analysis)
+                self.add_Ft_comb(ft_analysis, Fts)
+                print('')
+            else:
                 print('')
 
     # Generate ppm values and add to existing derived data session
@@ -118,35 +135,43 @@ class TRaILicpms(BaseImporter):
         radionuclides = [d for d in raw_data if d['type']['unit'] == 'ng']
         
         # Generate ppm values
-        ppm_data = []
         eU = 0
         eU_err = []
+        self.ppms = {}
         
         for r in radionuclides:
             if 'U' in r['type']['parameter']:
                 ppm_dict = make_ppm(r, dim_mass_val, dim_mass_err)
-                # ppm_data.append(ppm_dict)
                 ppm_dict['analysis'] = analysis_obj
+                self.ppms[r['type']['parameter']] = ppm_dict
                 self.db.load_data('datum', ppm_dict)
                 eU += ppm_dict['value']
-                eU_err.append(ppm_dict['error']**2)
+                eU_err.append((ppm_dict['error']/2)**2)
             elif 'Th' in r['type']['parameter']:
                 ppm_dict = make_ppm(r, dim_mass_val, dim_mass_err)
                 ppm_dict['analysis'] = analysis_obj
-                # ppm_data.append(ppm_dict)
+                self.ppms[r['type']['parameter']] = ppm_dict
                 self.db.load_data('datum', ppm_dict)
                 eU += 0.238*ppm_dict['value']
-                eU_err.append((0.238*ppm_dict['error'])**2)
+                eU_err.append((0.238*(ppm_dict['error']/2))**2)
             elif 'Sm' in r['type']['parameter']:
                 ppm_dict = make_ppm(r, dim_mass_val, dim_mass_err)
                 ppm_dict['analysis'] = analysis_obj
-                # ppm_data.append(ppm_dict)
                 self.db.load_data('datum', ppm_dict)
                 eU += 0.0012*ppm_dict['value']
-                eU_err.append((0.0012*ppm_dict['error'])**2)
-        ppm_data.append({'value': eU, 'error': sum(eU_err)**(1/2),
-                         'type': {'parameter': 'eU', 'unit': 'ppm'},
-                         'analysis': analysis_obj})
-        self.db.load_data('datum', ppm_dict)
+                eU_err.append((0.0012*(ppm_dict['error']/2))**2)
+        eU_dict = {'value': eU, 'error': eU*0.15, #sum(eU_err)**(1/2),
+                   'type': {'parameter': 'eU', 'unit': 'ppm'},# (±2σ)', 'unit': 'ppm'},
+                   'analysis': analysis_obj}
+        self.db.load_data('datum', eU_dict)
 
-        print('')
+    def add_Ft_comb(self, analysis_obj, Fts):
+        a_238 = (1.04+0.247*(self.ppms['232Th (±2σ)']['value']/self.ppms['238U (±2σ)']['value']))**-1
+        a_232 = (1.+4.21*(self.ppms['238U (±2σ)']['value']/self.ppms['232Th (±2σ)']['value']))**-1
+        Ft_comb = (a_238*float(Fts['238U Ft (±2σ)'].value) +
+                   a_232*float(Fts['232Th Ft (±2σ)'].value) +
+                   (1-a_238-a_232)*float(Fts['235U Ft (±2σ)'].value))
+        Ft_comb_dict= {'value': Ft_comb, 'error': None,
+                       'type': {'parameter': 'Combined Ft', 'unit': ''},
+                       'analysis': analysis_obj}
+        self.db.load_data('datum', Ft_comb_dict)
