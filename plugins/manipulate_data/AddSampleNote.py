8df2e6feaabc70068cbaf5ea52fb6221d40bfbd4
Peter E. Martin <76170591+Peter-E-Martin@users.noreply.github.com>
2022-06-20 15:46:34 -0600

    Improve user feedback for add-note plugin

--- plugins/manipulate_data/AddSampleNote.py
+++ plugins/manipulate_data/AddSampleNote.py
@@ -13,13 +13,18 @@ class AddSampleNote(BaseImporter):
     def __init__(self, app, data_dir, id_, note, **kwargs):
         super().__init__(app)
         if not id_ and not note:
-            sample_id = str(input('Sample ID to add note to: '))
-            add_note = str(input('Enter note: '))
+            try:
+                sample_id = str(input('Sample ID to add note to: '))
+                add_note = str(input('Enter note: '))
+            except:
+                print('Please enter the Sample ID and Note to add.')
+                return
         else:
             sample_id = id_
             add_note = note
         try:
             sample_obj = self.db.session.query(self.db.model.sample).filter_by(lab_id=sample_id).all()[0]
+            print('Adding note to sample', sample_obj.name)
         except IndexError:
             print('No sample with ID '+sample_id+' found.')
             return
