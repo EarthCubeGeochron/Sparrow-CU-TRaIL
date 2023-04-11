736efe887372aba6c6a1c6b1abfeedc8467fb7ef
Daven Quinn <dev@davenquinn.com>
2023-04-11 13:38:04 -0500

    Made prestart script more predictable

--- sparrow-prestart.sh
+++ sparrow-prestart.sh
@@ -2,7 +2,5 @@
 # (this only works in Sparrow >= 2.0.0.beta14, and we might find a better way
 #  to do it in the future)
 echo "Building custom backend image..." >&2
-if [ $SPARROW_ENV != "production" ]; then
-  sparrow build backend
-fi
+sparrow build backend
 docker build -t $SPARROW_BACKEND_IMAGE base-image
