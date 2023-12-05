beba0721d3d4f2663c3e8598412f0b1afd60c88c
Daven Quinn <dev@davenquinn.com>
2023-12-05 14:46:33 -0700

    Updated sparrow prestart script to restart Google Drive, added information about potential errors

--- restart-gdrive.sh
+++ restart-gdrive.sh
@@ -2,8 +2,19 @@
 # Temporary script to restart the Sparrow GDrive mount.
 
 mountpt=/projects/Sparrow-gdrive
-sudo rm -rf $mountpt # Requires password
-mkdir $mountpt
-chmod 775 $mountpt
+#sudo rm -rf $mountpt # Requires password
+
+if [ ! -d $mountpt ]; then
+	mkdir $mountpt
+	chmod 775 $mountpt
+fi
+
+# --allow-other: allows other users to connect to this mountpoint (a must)
+# fusermount: option allow_other only allowed if 'user_allow_other' is set in /etc/fuse.conf
+
+# Clear existing mount configuration, if any
+fusermount -uz $mountpt
+
+# Run the mount daemon
 rclone mount Sparrow_gdrive: $mountpt --allow-other --daemon
 
--- sparrow-prestart.sh
+++ sparrow-prestart.sh
@@ -1,6 +1,18 @@
 # Build the backend image before we run sparrow up
 # (this only works in Sparrow >= 2.0.0.beta14, and we might find a better way
 #  to do it in the future)
-echo "Building custom backend image..." >&2
-sparrow build backend
-docker build -t $SPARROW_BACKEND_IMAGE base-image
+echo "Restarting Google Drive mount." >&2
+bash restart-gdrive.sh
+if [ $? -ne 0 ]; then
+  echo "Google Drive mount was not created properly!" >&2
+  echo "If the system was recently updated, try setting the 'user_allow_other' option in /etc/fuse.conf" >&2
+fi
+echo "" >&2
+
+if [ -z SPARROW_BACKEND_IMAGE ]; then
+	echo "Custom backend image not provided" >&2
+else
+	echo "Building custom backend image..." >&2
+	sparrow build backend
+	docker build -t $SPARROW_BACKEND_IMAGE base-image
+fi
