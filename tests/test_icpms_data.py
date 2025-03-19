6999fe1e5626adf34eecffdc2249492ac319719a
Daven Quinn <code@davenquinn.com>
2025-03-19 19:55:21 -0400

    Successfully load all data files

--- tests/test_icpms_data.py
+++ tests/test_icpms_data.py
@@ -5,7 +5,7 @@ Integrated tests of ICP-MS and picking calculations
 # Add plugins to sys.path
 from pathlib import Path
 
-from pandas import read_excel
+from pandas import read_excel, read_csv
 import numpy as N
 
 from plugins.import_data.pickingImport import (
@@ -28,5 +28,27 @@ def test_picking_data_ingestion():
     df = read_excel(picking_sheet, header=1)
     df = df.iloc[4:, :]
     df = df[df["Lab/Owner"].notna()]
+
+    assert len(df) == 31
+
+
+def test_icpms_data_ingestion():
+    icpms_sheet = picking_data / "icpms_test" / "ICPMS_Data_Test.txt"
+    df = read_csv(icpms_sheet, delimiter="\t")
+
+    assert len(df) == 31
+
+
+def test_he_data_ingestion():
+    he_sheet = picking_data / "icpms_test" / "He_Data_Test.txt"
+    df = read_csv(he_sheet, delimiter="\t")
+
+    assert len(df) == 31
+
+
+def test_load_results():
+    results_sheet = picking_data / "icpms_test" / "Test_Data_Results.xlsx"
+    df = read_excel(results_sheet)
+    df = df[df.iloc[:, 0].notna()]
 
     assert len(df) == 31
